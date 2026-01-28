efe3f75d8b792de50d3a97992304c552ccc3b89c
saurabh7489 <saurabhpandey7489@gmail.com>
2026-01-28 23:55:24 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
  let canvas=   document.querySelector("canvas")
   let pen=  canvas.getContext("2d")
-   pen.fillStyle='gold'
+   // pen.fillStyle='gold'
    let cell=50
    let cellQ=[[0,0]]
+   let count=0
    let gameOver=false
    let direction='right'
    let randomC=generateRandomCell()
@@ -31,14 +32,16 @@
         clearInterval(id)
         return;
     }
-       pen.fillStyle='#dda15e'
+       pen.fillStyle='#fb8500'
     pen.clearRect(0,0,1000,600)
     for(let i of cellQ){
         pen.fillRect(i[0],i[1],cell,cell)
     }
     pen.font='20px sans-sarif'
-    pen.fillText('score ${count}',40,100)
-    pen.fillStyle='#fefae0'
+   pen.fillStyle='#ffffff'
+    pen.fillText(`score : ${count}`,40,100)
+    
+    pen.fillStyle='#dda15e'
     pen.fillRect(randomC[0],randomC[1],cell,cell)
 
    }
@@ -49,50 +52,54 @@
  let y=   cellQ[cellQ.length-1][1]
  let newX
  let nexY
- if(direction=='right'){
+ if(direction=='right' ){
     newX=x+cell;
     nexY=y
-    if(newX==900){
+    if(newX==900|| checkmate(newX,nexY)){
         gameOver=true
          pen.font='20px sans-sarif'
+          pen.fillStyle='red'
     pen.fillText('GAME OVER',40,120)
-      pen.fillStyle='red'
+     
     }
  }
  else if(direction=='left'){
     newX=x-cell;
     nexY=y
-    if(newX<0){
+    if(newX<0|| checkmate(newX,nexY)){
         gameOver=true
  pen.font='20px sans-sarif'
+  pen.fillStyle='red'
     pen.fillText('GAME OVER',40,120)
-      pen.fillStyle='red'
+     
     }
  }
  else if(direction=='down'){
     newX=x
     nexY=y+cell
-    if(nexY==500){
+    if(nexY==500|| checkmate(newX,nexY)){
         gameOver=true
          pen.font='20px sans-sarif'
+         pen.fillStyle='red'
     pen.fillText('GAME OVER',40,120)
-      pen.fillStyle='red'
+      
     }
  }
  else{
     newX=x
     nexY=y-cell
-    if(nexY<0){
+    if(nexY<0|| checkmate(newX,nexY)){
         gameOver=true
          pen.font='20px sans-sarif'
+          pen.fillStyle='red'
     pen.fillText('GAME OVER',40,120)
-      pen.fillStyle='red'
+     
     }
  }
 
  if(newX==randomC[0]  && nexY==randomC[1]){
     randomC=generateRandomCell()
-    
+    count=count+1
  }
  else{
     cellQ.shift()
@@ -109,4 +116,12 @@
             Math.floor(Math.random()*650/50)*50,
             Math.floor(Math.random()*350/50)*50
         ]
+     }
+     function checkmate(newX,nexY){
+      for(let i of cellQ){
+         if(i[0]==newX && i[1]==nexY){
+            return true;
+         }
+      }
+      return false
      }
